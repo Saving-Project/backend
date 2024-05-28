581666f0cde434a8f81e6bc2f05c4d9483bbcf81
SebastianSamboni <sebas_18_08@hotmail.com>
2024-05-28 08:33:44 -0500

    Create method and route to get user info

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,7 @@
 import { Router } from 'express'
+import { authRequired } from '../middlewares/checkToken.js'
 import {
+    getInfo,
     login,
     register
 } from '../controllers/user.controller.js'
@@ -8,5 +10,6 @@ const router = Router()
 
 router.post('/user', register)
 router.post('/login', login)
+router.get('/user', authRequired, getInfo)
 
 export default router
