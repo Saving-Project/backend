3a7105fd9f002165cb263293053732477232d666
SebastianSamboni <sebas_18_08@hotmail.com>
2024-06-04 07:19:36 -0500

    Fix error with dates in saving controller

--- src/controllers/saving.controller.js
+++ src/controllers/saving.controller.js
@@ -1,6 +1,7 @@
 import { Plan } from '../models/plan.model.js'
 import { DayPlan } from '../models/day_plan.model.js'
 import { SavingDay } from '../models/saving_day.model.js'
+import { sequelize } from '../database/database.js'
 
 export const createSavingsPlan = async (req, res) => {
     const user_id = req.user.id
@@ -77,6 +78,67 @@ export const getPlans = async (req, res) => {
         if (!plans) return res.status(404).json({ message: 'Planes no encontrados' })
         
         return res.status(200).json(plans)
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json(error)
+    }
+}
+
+export const markDayAsSaved = async (req, res) => {
+    const { id } = req.params
+    const user_id = req.user.id
+    const { day } = req.body
+
+    try {
+        console.log(`Id del plan: ${id} - Id del usuario: ${user_id}`)
+        const plan = await Plan.findOne({
+            where: {
+                id,
+                user_id
+            },
+            include: [{
+                model: DayPlan,
+                include: [SavingDay],
+                order: [['date', 'ASC']]
+            }]
+        })
+
+        if (!plan) return res.status(404).json({ message: 'Plan no encontrado' })
+        
+        const dayPlan = plan.day_plans.find(dp => dp.id === parseInt(day))
+        if (!dayPlan) return res.status(400).json({ message: 'DayPlan not found' })
+        
+        if (dayPlan.saved) return res.status(400).json({ message: 'Este día ya fue guardado' })
+
+        const currentDate = new Date().toISOString().split('T')[0];
+        if (dayPlan.date !== currentDate) {
+            return res.status(400).json({ message: 'No puedes guardar este día todavía' });
+        }
+        
+        const transaction = await sequelize.transaction()
+
+        try {
+            dayPlan.saved = true
+            const updateDay = await dayPlan.save({ transaction })
+
+            plan.total_saving = parseInt(plan.total_saving) + dayPlan.saving_day.amount
+            await plan.save({ transaction })
+            
+            const nextDayPlan = plan.day_plans.find(dp => dp.date > dayPlan.date)
+
+            if (nextDayPlan && nextDayPlan.date === new Date().toISOString().split('T')[0]) {
+                nextDayPlan.enabled = true
+                const nextDay = await nextDayPlan.save({ transaction })
+            }
+
+            await transaction.commit()
+
+            res.status(200).json({ message: 'Day marked as saved', plan })
+        } catch (error) {
+            await transaction.rollback()
+            console.error('Error marking day as saved: ', error)
+            res.status(500).json({ message: 'Internal server error' })
+        }
     } catch (error) {
         console.error(error)
         return res.status(500).json(error)
--- src/routes/saving.routes.js
+++ src/routes/saving.routes.js
@@ -3,7 +3,8 @@ import { authRequired } from '../middlewares/checkToken.js'
 import {
     createSavingsPlan,
     getPlan,
-    getPlans
+    getPlans,
+    markDayAsSaved
 } from '../controllers/saving.controller.js'
 
 const router = Router()
@@ -11,5 +12,6 @@ const router = Router()
 router.post('/saving', authRequired, createSavingsPlan)
 router.get('/saving/:id', authRequired, getPlan)
 router.get('/savings', authRequired, getPlans)
+router.put('/saving/:id', authRequired, markDayAsSaved)
 
 export default router
